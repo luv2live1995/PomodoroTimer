545922d91bf219649b675b22a7242558a30b8b85
Le Dinh Hoang Son <blackpantherluv2code@gmail.com>
2022-02-15 23:54:00 +0900

    Init

---
+++ BackupPro/PomodoroTimer.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace BackupPro
+{
+    partial class PomodoroTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.shownTimeText = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.longBreakTab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.longBreakTab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // shownTimeText
+            // 
+            this.shownTimeText.BackColor = System.Drawing.SystemColors.InfoText;
+            this.shownTimeText.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.shownTimeText.ForeColor = System.Drawing.Color.White;
+            this.shownTimeText.Location = new System.Drawing.Point(50, 82);
+            this.shownTimeText.Name = "shownTimeText";
+            this.shownTimeText.Size = new System.Drawing.Size(693, 67);
+            this.shownTimeText.TabIndex = 0;
+            this.shownTimeText.Text = "25:00";
+            this.shownTimeText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.shownTimeText.TextChanged += new System.EventHandler(this.shownTimeText_TextChanged);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(50, 192);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(112, 48);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(210, 192);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(112, 48);
+            this.stopBtn.TabIndex = 2;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(365, 192);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(112, 48);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // longBreakTab
+            // 
+            this.longBreakTab.Controls.Add(this.tabPage2);
+            this.longBreakTab.Controls.Add(this.tabPage1);
+            this.longBreakTab.Controls.Add(this.tabPage3);
+            this.longBreakTab.Location = new System.Drawing.Point(12, 13);
+            this.longBreakTab.Name = "longBreakTab";
+            this.longBreakTab.SelectedIndex = 0;
+            this.longBreakTab.Size = new System.Drawing.Size(776, 425);
+            this.longBreakTab.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.startBtn);
+            this.tabPage1.Controls.Add(this.resetBtn);
+            this.tabPage1.Controls.Add(this.shownTimeText);
+            this.tabPage1.Controls.Add(this.stopBtn);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 396);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 396);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(768, 396);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // PomodoroTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.longBreakTab);
+            this.Name = "PomodoroTimer";
+            this.Text = "PomodoroTimer";
+            this.longBreakTab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox shownTimeText;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.TabControl longBreakTab;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ BackupPro/PomodoroTimer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackupPro
+{
+    public partial class PomodoroTimer : Form
+    {
+        // 25 minutes = 1500 seconds
+        int POMODORO_WORK_MINUTES = 25;
+        int TWENTYFIVE_MINUTES_TO_SECONDS = 1500;
+        string DEFAULT_CLOCK_STRING = "25:00";
+
+        // Vars
+        int secondsCount;
+        DateTime defaultTime;
+
+        // When user clicks Stop Button Flag
+        int stopFlag = 0;
+
+        public PomodoroTimer()
+        {
+            InitializeComponent();
+            secondsCount = TWENTYFIVE_MINUTES_TO_SECONDS;
+            defaultTime = new DateTime().AddMinutes(POMODORO_WORK_MINUTES).AddSeconds(00);
+        }
+
+        // Clock text
+        private void shownTimeText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Start button
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            //DateTime currTime = new DateTime();
+            //timer1.Tick += new EventHandler(substract1Sec);
+            stopFlag = 0;
+            substract1Sec();
+        }
+
+        // Extract 1 second 
+        private void substract1Sec()
+        {
+            while (secondsCount >= 1)
+            {
+                DateTime now = DateTime.Now;
+
+                if (stopFlag == 0)
+                {
+                    defaultTime = defaultTime.AddSeconds(-1);
+
+                    if (defaultTime.Second < 10)
+                    {
+                        this.shownTimeText.Text = defaultTime.Minute.ToString()
+                                         + ":0" + defaultTime.Second.ToString();
+                    }
+                    else
+                    {
+                        this.shownTimeText.Text = defaultTime.Minute.ToString()
+                                         + ":" + defaultTime.Second.ToString();
+                    }
+                    --secondsCount;
+                    wait(1000);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        // Waiting 1 second
+        public void wait(int milliseconds)
+        {
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Start wait timer 
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Stop wait timer
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        // Stop button
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            stopFlag = 1;
+        }
+
+        // Reset button
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            secondsCount = TWENTYFIVE_MINUTES_TO_SECONDS;
+            this.shownTimeText.Text = DEFAULT_CLOCK_STRING;
+            defaultTime = new DateTime().AddMinutes(POMODORO_WORK_MINUTES).AddSeconds(00);
+        }
+
+    }
+}
